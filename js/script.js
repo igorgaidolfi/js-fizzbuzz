b668010c73d0f91e4e66b54fff0a394169044f70
igorgaidolfi <gandalf.4@outlook.it>
2023-11-22 17:25:34 +0100

    update cycle for only fizz

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-for(let i=1; i<100; i++){
+for(let i=1; i<=100; i++){
     let fizz
     if(i%3 == 0 ){
         console.log('fizz')
-        i+=1
+    }else{
+    console.log(i)
     }
-    if(i%5 == 0 ){
-        console.log('buzz')
-        i+=1
-    }
-    //console.log(i)
-    
 }
