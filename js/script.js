ec6c951b079b5e904d0a85b2fac16860c5e65d78
igorgaidolfi <gandalf.4@outlook.it>
2023-11-22 23:00:29 +0100

    update Bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+const list = document.querySelector('ul')
 for(let i=1; i<=100; i++){
     if(i%15==0){
-        console.log('FizzBuzz')
+        let li = `<li style="list-style: none">FizzBuzz</li>`
+        list.innerHTML += li
     }
     else if(i%3 == 0){
-        console.log('Fizz')
+        let li = `<li style="list-style: none">Fizz</li>`
+        list.innerHTML += li
     }else if(i%5==0){
-        console.log('Buzz')
+        let li = `<li style="list-style: none">Buzz</li>`
+        list.innerHTML += li
     }else{
-    console.log(i)
+        let li = `<li style="list-style: none"> ${i} </li>`
+        list.innerHTML += li
     }
-    
 }
