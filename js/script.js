f3cd6be8ac9d91d50a78b796ecb6527e4a2ca6b3
igorgaidolfi <gandalf.4@outlook.it>
2023-11-22 17:20:41 +0100

    update Edited script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+for(let i=1; i<100; i++){
+    let fizz
+    if(i%3 == 0 ){
+        console.log('fizz')
+        i+=1
+    }
+    if(i%5 == 0 ){
+        console.log('buzz')
+        i+=1
+    }
+    //console.log(i)
+    
+}
