f2bfac858109779a88ad354251ff0dff8d83aaee
igorgaidolfi <gandalf.4@outlook.it>
2023-11-22 17:29:45 +0100

    update loop with fizz and buzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 for(let i=1; i<=100; i++){
     let fizz
-    if(i%3 == 0 ){
+    if(i%3 == 0){
         console.log('fizz')
+    }else if(i%5==0){
+        console.log('buzz')
     }else{
     console.log(i)
     }
+    
 }
